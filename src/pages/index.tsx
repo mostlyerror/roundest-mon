c74a780261aed61b13d450ff26804e7721645627
Ben Poon <benjamintpoon@gmail.com>
2023-04-12 17:32:25 -0500

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <div className="">Hello World</div>
+  );
+};
+
+export default Home;
